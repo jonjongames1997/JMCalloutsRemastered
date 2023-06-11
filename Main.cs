b6013190b6e975221f3e3d7212015874d86cf5b1
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2023-06-10 20:52:07 -0400

    Modified

--- Main.cs
+++ Main.cs
@@ -1,80 +1,62 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Linq;
-using System.Threading.Tasks;
-using Rage;
+﻿using Rage;
 using LSPD_First_Response.Mod.API;
+using JMCalloutsRemastered.Callouts;
+using JMCalloutsRemastered.VersionChecker;
+using JMCalloutsRemastered;
 using System.Reflection;
 
 namespace JMCalloutsRemastered
 {
     public class Main : Plugin
     {
-
-        public static String Version = "2.3.1";
+        public override void Finally() { }
 
         public override void Initialize()
         {
-            Functions.OnOnDutyStateChanged += OnOnDutyStateChangedHandler;
-            Game.LogTrivial("Plugin JM Callouts Remastered" + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString() + " by OfficerMorrison has been initialized!");
-            Game.LogTrivial("Go On Duty to fully load JM Callouts Remastered");
-
-            AppDomain.CurrentDomain.AssemblyResolve += new ResolveEventHandler(LSPDFRResolveEventHandler);
+            Functions.OnOnDutyStateChanged += Functions_OnOnDutyStateChanged;
+            Settings.LoadSettings();
         }
-
-        public override void Finally()
+        static void Functions_OnOnDutyStateChanged(bool onDuty)
         {
-            Game.LogTrivial("JM Callouts has been cleaned up!");
-        }
-
-        private static void OnOnDutyStateChangedHandler(bool OnDuty)
-        {
-            if (OnDuty)
-            {
-                RegisterCallouts();
-                Game.DisplayNotification("JM Callouts Remastered by OfficerMorrison | ~y~Version 2.3.1 | BETA | has been successfully loaded");
-            }
-        }
-
-        private static void RegisterCallouts()
-        {
-            Functions.RegisterCallout(typeof(Callouts.IntoxicatedIndividual));
-            Functions.RegisterCallout(typeof(Callouts.IllegalCampfireOnPublicBeach));
-            Functions.RegisterCallout(typeof(Callouts.TrespassingOnPrivateProperty));
-            Functions.RegisterCallout(typeof(Callouts.RefuseToLeave));
-            Functions.RegisterCallout(typeof(Callouts.PossibleProstitution));
-            Functions.RegisterCallout(typeof(Callouts.RefuseToPay));
-            Functions.RegisterCallout(typeof(Callouts.Soliciting));
-        }
-
-        public static Assembly LSPDFRResolveEventHandler(object sender, ResolveEventArgs args)
-        {
-            foreach (Assembly assembly in Functions.GetAllUserPlugins())
-            {
-                if (args.Name.ToLower().Contains(assembly.GetName().Name.ToLower()))
+            if (onDuty)
+                GameFiber.StartNew(delegate
                 {
-                    return assembly;
-                }
-            }
-            return null;
-        }
+                    RegisterCallouts();
+                    Game.Console.Print();
+                    Game.Console.Print("=============================================== JMCalloutsRemastered by OfficerMorrison ================================================");
+                    Game.Console.Print();
+                    Game.Console.Print("[LOG]: Callouts and settings were loaded successfully.");
+                    Game.Console.Print("[LOG]: The config file was loaded successfully.");
+                    Game.Console.Print("[VERSION]: Detected Version: " + Assembly.GetExecutingAssembly().GetName().Version.ToString());
+                    Game.Console.Print("[LOG]: Checking for a new JMCalloutsRemastered version...");
+                    Game.Console.Print();
+                    Game.Console.Print("=============================================== JMCalloutsRemastered by OfficerMorrison ================================================");
+                    Game.Console.Print();
 
-        public static bool IsLSPDFRPluginIsRunning(string Plugin, Version minversion = null)
-        {
-            foreach(Assembly assembly in Functions.GetAllUserPlugins())
-            {
-                AssemblyName an = assembly.GetName();
-                if(an.Name.ToLower() == Plugin.ToLower())
-                {
-                    if(minversion == null || an.Version.CompareTo(minversion) >= 0)
+                    // You can find all textures/images in OpenIV
+                    Game.DisplayNotification("web_lossantospolicedept", "web_lossantospolicedept", "UnitedCallouts", "~y~v" + Assembly.GetExecutingAssembly().GetName().Version.ToString() + " ~o~by sEbi3", "~b~successfully loaded!");
+                    // Game.DisplayNotification("web_lossantospolicedept", "web_lossantospolicedept", "UnitedCallouts", "~y~Unstable Build", "This is an ~r~unstable build~w~ of UnitedCallouts for testing. You may notice bugs while playing the unstable build.");
+
+                    PluginCheck.isUpdateAvailable();
+                    GameFiber.Wait(300);
+                    if (Settings.HelpMessages)
                     {
-                        return true;
+                        Game.DisplayHelp("You can change all ~y~keys~w~ in the ~g~UnitedCallouts.ini~w~. Press ~b~" + Settings.EndCall + "~w~ to end a callout.", 5000);
                     }
-                }
-            }
-            return false;
+                    else { Settings.HelpMessages = false; }
+                });
         }
-
+        private static void RegisterCallouts() //Register all your callouts here
+        {
+            Game.Console.Print();
+            Game.Console.Print("================================================== UnitedCallouts ===================================================");
+            Game.Console.Print();
+            if (Settings.IllegalCampfireOnPublicBeach) { Functions.RegisterCallout(typeof(IllegalCampfireOnPublicBeach)); }
+           
+            Game.Console.Print("[LOG]: All callouts of the JMCalloutsRemastered.ini were loaded successfully.");
+            Game.Console.Print();
+            Game.Console.Print("================================================== UnitedCallouts ===================================================");
+            Game.Console.Print();
+        }
     }
 }
--- VersionChecker/VersionChecker.cs
+++ VersionChecker/VersionChecker.cs
@@ -4,55 +4,50 @@ using System.Net;
 
 namespace JMCalloutsRemastered.VersionChecker
 {
-    public class VersionChecker
+    public class PluginCheck
     {
-
         public static bool isUpdateAvailable()
         {
-            string curVersion = Settings.CalloutVersion;
-
-            Uri latestVersionUri = new Uri("https://www.lcpdfr.com/applications/downloadsng/interface/api.php?do=checkForUpdates&fileId=43616&textOnly=1");
+            string curVersion = Settings.PluginVersion;
+            Uri latestVersionUri = new Uri("https://www.lcpdfr.com/downloads/gta5mods/scripts/43616-jm-callouts-remastered-beta-now-open-source-for-callout-devs/");
             WebClient webClient = new WebClient();
             string receivedData = string.Empty;
-
             try
             {
-                receivedData = webClient.DownloadString("https://www.lcpdfr.com/applications/downloadsng/interface/api.php?do=checkForUpdates&fileId=43616&textOnly=1").Trim();
+                receivedData = webClient.DownloadString(latestVersionUri).Trim();
             }
             catch (WebException)
             {
-                Game.DisplayNotification("commonmenu", "mp_alerttriangle", "~w~JMCalloutsRemastered Warning", "~y~Failed to check for a update", "Please check if you are ~o~online~w~, or try to reload the plugin.");
-
+                Game.DisplayNotification("commonmenu", "mp_alerttriangle", "~w~JMCalloutsRemastered Warning", "~r~Failed to check for an update", "Please make sure you are ~y~connected~w~ to the internet or try to ~y~reload~w~ the plugin.");
                 Game.Console.Print();
-                Game.Console.Print("================================================ JMCalloutsRemastered WARNING =====================================================");
+                Game.Console.Print("================================================== JMCalloutsRemastered ===================================================");
                 Game.Console.Print();
-                Game.Console.Print("Failed to check for a update.");
-                Game.Console.Print("Please check if you are online, or try to reload the plugin.");
+                Game.Console.Print("[WARNING]: Failed to check for an update.");
+                Game.Console.Print("[LOG]: Please make sure you are connected to the internet or try to reload the plugin.");
                 Game.Console.Print();
-                Game.Console.Print("================================================ JMCalloutsRemastered WARNING =====================================================");
+                Game.Console.Print("================================================== JMCalloutsRemastered ===================================================");
                 Game.Console.Print();
+                return false;
             }
-            if (receivedData != Settings.CalloutVersion)
+            if (receivedData != Settings.PluginVersion)
             {
-                Game.DisplayNotification("commonmenu", "mp_alerttriangle", "~w~ExampleCallouts Warning", "~r~A new Update is available!", "Current Version: ~r~" + curVersion + "~w~<br>New Version: ~g~" + receivedData);
-
+                Game.DisplayNotification("commonmenu", "mp_alerttriangle", "~w~JMCalloutsRemastered Warning", "~y~A new Update is available!", "Current Version: ~r~" + curVersion + "~w~<br>New Version: ~o~" + receivedData + "<br>~r~Please update to the latest build!");
                 Game.Console.Print();
-                Game.Console.Print("================================================ JMCalloutsRemastered WARNING =====================================================");
+                Game.Console.Print("================================================== JMCalloutsRemastered ===================================================");
                 Game.Console.Print();
-                Game.Console.Print("A new version of JMCalloutsRemastered is available! Update the Version, or play on your own risk.");
-                Game.Console.Print("Current Version:  " + curVersion);
-                Game.Console.Print("New Version:  " + receivedData);
+                Game.Console.Print("[WARNING]: A new version of UnitedCallouts is available! Update to the latest build or play on your own risk.");
+                Game.Console.Print("[LOG]: Current Version:  " + curVersion);
+                Game.Console.Print("[LOG]: New Version:  " + receivedData);
                 Game.Console.Print();
-                Game.Console.Print("================================================ JMCalloutsRemastered WARNING =====================================================");
+                Game.Console.Print("================================================== JMCalloutsRemastered ===================================================");
                 Game.Console.Print();
                 return true;
             }
             else
             {
-                Game.DisplayNotification("web_lossantospolicedept", "web_lossantospolicedept", "~w~JMCalloutsRemastered Information", "~g~You are playing on the newest version!", "Installed Version: ~g~" + curVersion + "");
+                Game.DisplayNotification("web_lossantospolicedept", "web_lossantospolicedept", "~w~JMCalloutsRemastered", "", "Detected the ~g~latest~w~ build of ~y~JMCalloutsRemastered~w~!");
                 return false;
             }
         }
-
     }
 }
